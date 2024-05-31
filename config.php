68fb721a52ffae4fa131e612582ae15a39bb2f57
Devon Matsalla <dpmatsalla@hotmail.com>
2024-05-31 20:11:54 +1000

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+// config.php
+<?php
+return [
+    'clientId' => 'YOUR_CLIENT_ID',
+    'clientSecret' => 'YOUR_CLIENT_SECRET',
+    'redirectUri' => 'http://yourdomain.com/callback.php',
+    'scopes' => 'Files.Read User.Read',
+];
